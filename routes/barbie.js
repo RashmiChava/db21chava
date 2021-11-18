00a0fcde526a85bd22800853c3d4ca5dfc3443e7
RashmiChava <89612173+RashmiChava@users.noreply.github.com>
2021-11-17 22:19:03 -0600

    Adding the updated files

--- routes/barbie.js
+++ routes/barbie.js
@@ -1,6 +1,15 @@
 var express = require('express');
-const costume_controlers= require('../controllers/costume');
+const costume_controllers= require('../controllers/costume');
 var router = express.Router();
 /* GET costumes */
-router.get('/', costume_controlers.costume_view_all_Page );
+router.get('/', costume_controllers.costume_view_all_Page );
+
+router.get('/detail', costume_controllers.costume_view_one_Page); 
+
+router.get('/create', costume_controllers.costume_create_Page); 
+ 
+router.get('/update', costume_controllers.costume_update_Page);
+
+router.get('/delete', costume_controllers.costume_delete_Page); 
+
 module.exports = router;
